87d88a9eb36ee153507aa7ce074e4a44a4d9d32f
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2020-12-18 13:05:11 -0600

    Add randomizer functionality for radio buttons

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
+var sides = document.querySelector('#side');
+var mainDish = document.querySelector('#mainDish');
+var dessert = document.querySelector('#dessert');
 
+var letsCook = document.querySelector('#letsCook')
 
+sides.addEventListener('click', chooseSide)
+mainDish.addEventListener('click', chooseMain)
+dessert.addEventListener('click', chooseDessert)
+letsCook.addEventListener('click', displayMeal)
 
-
-
+var randomMeal = ''
 
 function randomizer(foodCourse){
   return Math.floor(Math.random() * (meals[foodCourse].length-1))
+}
+
+function chooseSide() {
+  randomMeal = meals.sides[randomizer('sides')]
+}
+
+function chooseMain() {
+  randomMeal = meals.mains[randomizer('mains')]
+}
+
+function chooseDessert() {
+  randomMeal = meals.desserts[randomizer('desserts')]
+}
+
+function displayMeal() {
+
 }
